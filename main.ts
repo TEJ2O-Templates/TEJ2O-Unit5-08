a3d33342d9b363bd06dfd81b81a36aa1e2460e69
Patrick Coxall <mr.coxall@mths.ca>
2023-11-22 08:47:43 -0500

    init typescript

--- main.ts
+++ main.ts
@@ -2,7 +2,29 @@
  *
  * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program make a 28BYJ-48 DC 5V stepper motor move
+ * Also, using the new standard of using a while (true) statement
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.showIcon(IconNames.Happy)
+
+// loop forever
+while (true){
+  if (input.buttonIsPressed(Button.A) == true) {
+    // turn the motor 180 degrees
+    basic.showIcon(IconNames.Yes)
+    robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B0)
+    basic.pause(100)
+    basic.showIcon(IconNames.Happy)
+  }
+
+  if (input.buttonIsPressed(Button.B) == true) {
+    // move car forwards and backwards
+    basic.showIcon(IconNames.Yes)
+    robotbit.StpCarMove(10, 48)
+    basic.pause(500)
+    robotbit.StpCarMove(-10, 48)
+    basic.showIcon(IconNames.Sad)
+  }
+}
